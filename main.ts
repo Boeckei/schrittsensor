f275a0a261fb23e0423040e98aca6c7b4570c9fb
Boeckei <simon.seeboeck@web.de>
2024-12-18 10:09:06 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.showNumber(0)
+    schritte = 0
+})
 input.onGesture(Gesture.Shake, function () {
     schritte += 1
     basic.showNumber(schritte)
